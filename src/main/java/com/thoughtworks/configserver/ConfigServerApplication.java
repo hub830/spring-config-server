9ec598251f2e5e79e4ba9df5c6fb989ec16d82e5
Jim Arnold <jim@thoughtworks.com>
2015-12-15 10:57:10 -0500

    removed unnecessary dependencies

--- src/main/java/com/thoughtworks/configserver/ConfigServerApplication.java
+++ src/main/java/com/thoughtworks/configserver/ConfigServerApplication.java
@@ -4,9 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@Configuration
 @EnableConfigServer
 @EnableAutoConfiguration
 public class ConfigServerApplication {
